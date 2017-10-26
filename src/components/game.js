8fc44162386cfd4ba4d4abd45425167d78d4ce0e
barnettm <barnett717@gmail.com>
2017-10-26 10:42:26 -0700

    added css animation to response div

--- src/components/game.js
+++ src/components/game.js
@@ -8,7 +8,9 @@ class Game extends Component {
 
         this.state = {
             theNumber: this.getRandomNumber(),
-            theGuess: ''
+            theGuess: '',
+            guessResponse: '',
+            styleClass: ''
         };
         this.getRandomNumber = this.getRandomNumber.bind(this);
         this.reset = this.reset.bind(this);
@@ -16,69 +18,77 @@ class Game extends Component {
         this.handleNumberGuess = this.handleNumberGuess.bind(this);
     }
     getRandomNumber = function(){
-        return Math.floor(Math.random()*10)+1
+        return Math.floor(Math.random()*100)+1
     };
 
 
     reset = function(event){
         event.preventDefault();
-        console.log('reset clicked');
         this.setState({
             theNumber: this.getRandomNumber(),
-            theGuess: ''
+            theGuess: '',
+            guessResponse: ''
         })
     };
 
     handleNumberGuess = function(event){
         event.preventDefault();
-        console.log(this.state.theGuess);
-        let theGuess = this.state.theGuess;
-        let theNumber = this.state.theNumber;
+        const {theGuess, theNumber} = this.state;
+        // const {styleClass} = this.state.styleClass;
         if(theGuess == theNumber){
-            console.log("You guessed it!!")
+            this.setState({
+                guessResponse: 'You Guessed It!!'
+            })
         }else if (theGuess > theNumber){
-            console.log("Too High!")
+            this.setState({
+                guessResponse: 'Too High!!',
+                styleClass: 'shake'
+            })
+
         }else{
-            console.log('Too Low!')
+            this.setState({
+                guessResponse: 'Too Low!!',
+                styleClass: 'shake'
+            })
         }
         this.setState({
-            theGuess: ''
+            theGuess: '',
         })
     };
 
     handleInputChange = function(event){
         event.preventDefault();
         this.setState({
-            theGuess: event.target.value
+            theGuess: event.target.value,
+            styleClass: ''
         })
     };
-
-
-
 
 
     render(){
         console.log(this.state);
         const {theGuess} = this.state;
+        const{guessResponse} = this.state;
+        const{styleClass} = this.state;
         return (
             <div className="text-center">
                 <h1 className="my-3">Guessing Game</h1>
                 <form onSubmit={this.handleNumberGuess}>
-                    <h3>Guess A Number Between 1-10</h3>
-                    <input type="number" className="input-lg" onChange={this.handleInputChange} value={theGuess}/>
+                    <h3>Guess A Number Between 1-100</h3>
+                    <input type="number" className="input-lg text-center" onChange={this.handleInputChange} value={theGuess}/>
                     <div className="row justify-content-center">
                         <button className="btn btn-lg btn-outline-danger col-md-2 m-3 text-center" type='button' onClick={this.reset}>Reset</button>
                         <button className="btn btn-lg btn-outline-success col-md-2 m-3 text-center">Guess</button>
                     </div>
                 </form>
-                <div className="responseDiv">
-                    <h1>Too Low || Too High</h1>/* response div*/
+                <div className={styleClass}>
+                    <h1>{guessResponse}</h1>
                 </div>
                 <div className="historyDiv">
                     <ul className="list-group">
 
                     </ul>
-                </div> /*history div*/
+                </div>
             </div>
         )
     }
