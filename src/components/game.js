33cd607fc6cffbd0e7fc23daa446c223ca8c7049
barnettm <barnett717@gmail.com>
2018-01-24 13:03:40 -0800

    added higher order component

--- src/components/game.js
+++ src/components/game.js
@@ -1,127 +1,16 @@
-import React, { Component } from 'react';
-import History from './history';
-
-
-
-class Game extends Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            theNumber: this.getRandomNumber(),
-            theGuess: '',
-            guessResponse: '',
-            styleClass: '',
-            history: [],
-            guessCount: 0,
-            lowestScore: localStorage.getItem('lowestScore') || 'Not Set',
-        };
-        this.getRandomNumber = this.getRandomNumber.bind(this);
-        this.reset = this.reset.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleNumberGuess = this.handleNumberGuess.bind(this);
-        this.inputHistoryArray = this.inputHistoryArray.bind(this);
-        this.getLowestScore = this.getLowestScore.bind(this);
-    }
-    getRandomNumber = function(){
-        return Math.floor(Math.random()*100)+1
-    };
-
-
-    reset = function(event){
-        event.preventDefault();
-        this.setState({
-            theNumber: this.getRandomNumber(),
-            theGuess: '',
-            guessResponse: '',
-            history: [],
-            guessCount: 0,
-            success: ''
-            
-        })
-    };
-
-    handleNumberGuess = function(event){
-        event.preventDefault();
-        const {theGuess, theNumber, guessCount, success} = this.state;
-        // const {styleClass} = this.state.styleClass;
-        if(theGuess == undefined || theGuess == ''){
-            return
-        }
-        if(theGuess == theNumber){
-            this.setState({
-                guessResponse: 'You Guessed It!!',
-                success: 'success',
-                guessCount: this.state.guessCount +=1,
-            }, () => {
-                this.getLowestScore(guessCount)
-            })
-        }else if (theGuess > theNumber){
-            this.setState({
-                guessResponse: 'Too High!!',
-                styleClass: 'shake',
-                guessCount: this.state.guessCount += 1,
-            }, () => {
-                this.inputHistoryArray()
-            })
-        }else{
-            this.setState({
-                guessResponse: 'Too Low!!',
-                styleClass: 'shake',
-                guessCount: this.state.guessCount += 1,
-            }, () => {
-                this.inputHistoryArray()
-            })
-        }
-    };
-
-    handleInputChange(event){
-        event.preventDefault();
-        this.setState({
-            theGuess: event.target.value,
-            styleClass: ''
-        })
-    };
-
-    inputHistoryArray() {
-        const {history, theGuess, guessResponse } = this.state;
-        this.setState({
-            history: [`${theGuess} | ${guessResponse}`, ...history],
-            theGuess: ''
-        })
-    }
-
-    getLowestScore(score){
-        const lowestScore = localStorage.getItem('lowestScore');
-        if(!lowestScore || lowestScore > score){
-            localStorage.setItem('lowestScore', score);
-            this.setState({
-                lowestScore: score
-            })
-        }
-    }
-
-
-    render(){
-        console.log(this.state);
-        // const {theGuess} = this.state;
-        // const{guessResponse} = this.state;
-        // const{styleClass} = this.state;
-        // const{history} = this.state;
-        // const{guessCount} = this.state;
-        // const{lowestScore} = this.state;
-        // const{success} = this.state;
-        const{theGuess, guessResponse, styleClass, history, guessCount, lowestScore, success} = this.state;
-        const score = `Current Score: ${guessCount} | Top Score: ${lowestScore}`;
-        // const responseDiv = `responseDiv ${styleClass}`;
-        return (
+import React from "react";
+export default (props) => {
+    const  {theGuess, guessResponse, styleClass, history, guessCount, lowestScore, success, handleInputChange, handleNumberGuess, resetGame} = props;
+    console.log(props)
+    return (
+        <div>
             <div className="text-center main-area">
                 <h1 className="my-3">Guardians Guess</h1>
-                <form onSubmit={this.handleNumberGuess}>
+                <form onSubmit={handleNumberGuess}>
                     <h4>Guess A Number Between 1 and 100</h4>
-                    <input type="number" className="input-lg text-center" placeholder="1-100" onChange={this.handleInputChange} value={theGuess}/>
+                    <input type="number" className="input-lg text-center" placeholder="1-100" onChange={handleInputChange} value={theGuess}/>
                     <div className="row justify-content-center">
-                        <button className="btn btn-lg btn-outline-danger col-md-2 m-2 text-center reset" type='button' onClick={this.reset}>Reset</button>
+                        <button className="btn btn-lg btn-outline-danger col-md-2 m-2 text-center reset" type='button' onClick={resetGame}>Reset</button>
                         <button className="btn btn-lg btn-outline-success col-md-2 m-2 text-center guess">Guess</button>
                     </div>
                 </form>
@@ -129,12 +18,9 @@ class Game extends Component {
                     <h1 className={success}>{guessResponse}</h1>
                 </div>
                 <div>
-                    <p>{score}</p>
+                    <p>{guessCount}</p>
                 </div>
-                <History history={history} guessResponse={guessResponse}/>
-                </div>
-        )
-    }
+             </div>
+        </div>
+    )
 }
-
-export default Game;
