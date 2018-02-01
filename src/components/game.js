7a682764d77d90203702fa389a0f5d9e2d911e1b
barnettm <barnett717@gmail.com>
2018-02-01 11:56:58 -0800

    added smaller images and working on disabled button

--- src/components/game.js
+++ src/components/game.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 export default (props) => {
-    const  {theGuess, guessResponse, theNumber, styleClass, history, guessCount, lowestScore, success, handleInputChange, handleNumberGuess, resetGame} = props;
-    const score = `Current Score: ${guessCount} | Top Score: ${lowestScore}`;
+    const  {theGuess, guessResponse, theNumber, styleClass, history, guessCount,disabled, lowestScore, success, handleInputChange, handleNumberGuess, resetGame} = props;
+    const score = `Current Score: ${guessCount} | Top Score: ${lowestScore}`
+    const disable = `${disabled}`;
+
     return (
         <div>
             <div className="text-center main-area">
